ffd775aa81ddf2cb1fa9fe67f3a7ce2365dcee82
Courage Angeh <courageangeh@gmail.com>
2021-11-08 16:44:05 -0500

    Update build.sh
    add serverless deploy

--- build/build.sh
+++ build/build.sh
@@ -9,14 +9,19 @@ if [ "$ENV" = "dev" ]; then
    export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r .Credentials.AccessKeyId)
    export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r .Credentials.SecretAccessKey)
    export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r .Credentials.SessionToken)
-elif [ "$ENV" == "qa" ]; then
-   CREDS=$(aws sts assume-role --role-arn $qa_build_role --role-session-name sls-session --out json)
+   export STAGE=dev
+elif [ "$ENV" == "prod" ]; then
+   CREDS=$(aws sts assume-role --role-arn $prod_build_role --role-session-name sls-session --out json)
    export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r .Credentials.AccessKeyId)
    export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r .Credentials.SecretAccessKey)
    export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r .Credentials.SessionToken)
+   export STAGE=prod
 else
    echo "Unable to identify environment"
 fi
 
 echo "AccountID..........."
 aws sts get-caller-identity --query Account --output text
+
+echo "Running serverless deploy"
+sls deploy --stage=$STAGE --verbose
