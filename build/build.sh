6e83dde4b92ea0dfec1d8e4273f0d25137a3acdc
Courage Angeh <courageangeh@gmail.com>
2021-11-07 21:47:42 -0500

    Update build.sh
    update script

--- build/build.sh
+++ build/build.sh
@@ -4,12 +4,12 @@ set -e
 
 env
 
-if ["$ENV" = "dev"]; then
+if [ "$ENV" = "dev" ]; then
    CREDS=$(aws sts assume-role role-arn $dev_build_role --role-session-name sls-session --out json)
    export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r .Credentials.AccessKeyId)
    export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r .Credentials.SecretAccessKey)
    export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r .Credentials.SessionToken)
-elif ["$ENV" == "qa"]; then
+elif [ "$ENV" == "qa" ]; then
    CREDS=$(aws sts assume-role role-arn $qa_build_role --role-session-name sls-session --out json)
    export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r .Credentials.AccessKeyId)
    export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r .Credentials.SecretAccessKey)
